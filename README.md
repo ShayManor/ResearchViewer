428cef9b7bc0d26e3ed841d279f124814b0f60c4
shay <shay.manor@gmail.com>
2026-01-16 14:41:47 -0500

    feat(process_citations): Add getting author function

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Arxiv dataset analyzer for CS 348 (Databases)
 ## Features
 
 ### Paper Analyzer
-1) For each paper, get data on authors, citation number, keywords, journal, subject, and time
+1) For each paper (1,283,087 records), get data on authors, citation number, keywords, journal, subject, and time
 2) Allow sorting / analyzing to get metrics based on these units
 3) Make charts showing change over certain metrics
 4) See whole graph where edges are citations or author and colored by subject.
--- data/process_citations.py
+++ data/process_citations.py
@@ -1,6 +1,43 @@
 import duckdb
+import requests
+
+# 1) Get DOI for all papers from arxiv id
+# 2) Get author list and update papers table
+# 3) Get citations list from DOIs (of DOIs) and update papers table
 
 conn = duckdb.connect('../data.db')
-res = conn.execute('SELECT * FROM papers LIMIT 1;').fetchdf()
-print(res)
+res = conn.execute('SELECT count(*) FROM papers where ID is not null;').fetchdf()
 
+
+def get_author_ids(dois: str, *, prefer_orcid: bool = True) -> list[str]:
+    """
+    Returns an id for each author on each paper (doi)
+    - If prefer_orcid=True and ORCID exists, returns ORCID URL for that author
+    - Otherwise returns OpenAlex Author ID (https://openalex.org/A...)
+
+    NOTE: OpenAlex truncates authorships at 100 authors in the API.
+    """
+    url = "https://api.openalex.org/works"
+
+    filter_str = "|".join([f"doi:https://doi.org/{d}" for d in dois])  # dois = list of 50
+    r = requests.get(
+        url,
+        params={"filter": filter_str, "per-page": 50, "select": "doi,authorships"},
+        timeout=20
+    )
+
+    r.raise_for_status()
+    w = r.json()['results']
+
+    ids: list[str] = []
+    for a in w.get("authorships", []):
+        author = a.get("author", {}) or {}
+        openalex_id = author.get("id")      # unique author id
+        orcid = author.get("orcid")         # may be None
+
+        if prefer_orcid and orcid:
+            ids.append(orcid)
+        elif openalex_id:
+            ids.append(openalex_id)
+
+    return ids
